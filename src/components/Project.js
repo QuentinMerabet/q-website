3ae11fad7ad6fbcfb26f730567553f96dc2eaf3d
QuentinMerabet <bonjour@quentinmerabet.com>
2023-04-20 22:49:50 -0400

    arrows for project links

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,6 @@
 export default function Project(props) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col project">
       <h3>{props.title}</h3>
       <span className="subtext">{props.client}</span>
       <div className="mt-3 flex flex-col">{props.children}</div>
